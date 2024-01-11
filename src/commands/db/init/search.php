c12affe569c44ab3a9e404f07088e4137ab6cdd2
Thierry Graff <tigraff@gmail.com>
2024-01-11 10:53:24 +0100

    Fix a name error in generation of table search

--- src/commands/db/init/search.php
+++ src/commands/db/init/search.php
@@ -1,7 +1,7 @@
 <?php
 /******************************************************************************
     
-    Fills table search used by ajax
+    Recomputes completely table search used by ajax, for all persons stored in database.
     
     @license    GPL - conforms to file LICENCE located in root directory of current repository.
     @history    2021-07-22 15:30:56+02:00, Thierry Graff : Creation
@@ -9,7 +9,6 @@
 namespace g5\commands\db\init;
 
 use tiglib\patterns\Command;
-use g5\model\DB5;
 use g5\model\Search as ModelSearch;
 
 class search implements Command {
@@ -23,33 +22,7 @@ class search implements Command {
             return "USELESS PARAMETER {$params[0]}\n";
         }
         $report = "--- db init search ---\n";
-        
-        $dblink = DB5::getDbLink();
-        
-        $dblink->exec("delete from search");
-        
-        $stmt_insert = $dblink->prepare("insert into search(slug,day,name)values(?,?,?)");
-        
-        $N_person = 0;
-        $N_inserted = 0;
-        
-        $t1 = microtime(true);
-        $query = "select slug,name from person";
-        $stmt = $dblink->query($query);
-        foreach($stmt->fetchAll(\PDO::FETCH_ASSOC) as $row){
-            $slug = $row['slug'];
-            $json_name = json_decode($row['name'], true);
-            $names = ModelSearch::computePersonNames($json_name);
-            $bday = substr($slug, -10);
-            foreach($names as $name){
-                $stmt_insert->execute([$slug, $bday, $name]);
-                $N_inserted++;
-            }
-            $N_person++;
-        }
-        $t2 = microtime(true);
-        $dt = round($t2 - $t1, 3);
-        $report .= "Inserted $N_inserted lines for $N_person persons ($dt s)\n";
+        $report .= ModelSearch::addAllPersons();
         return $report;
     }
     
