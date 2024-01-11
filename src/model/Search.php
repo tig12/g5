46307e7b7d72dcde7ef16bda8cf0e40694a652f4
Thierry Graff <tigraff@gmail.com>
2024-01-11 11:13:01 +0100

    Add the notion of search term in search table

--- src/model/Search.php
+++ src/model/Search.php
@@ -9,8 +9,9 @@
 namespace g5\model;
 
 use g5\app\Config;
-
-Geonames::init();
+use g5\model\DB5;
+use g5\model\Person;
+use tiglib\strings\slugify;
 
 class Search {
     
@@ -22,7 +23,7 @@ class Search {
             'given' => Pierre
             'family' => Alard
             'nobl' => ''
-            'spouse' => '',
+            'spouse' => [],
             'official' => [
                 'given' => ''
                 'family' => ''
@@ -40,15 +41,23 @@ class Search {
         $res = [];
         // "normal" name
         $fam = $arrNames['family'];
-        if($arrNames['nobl'] != ''){
-            $fam = $arrNames['nobl'] . $fam;
-        }
-        if($fam != ''){
+        if($fam != ''){ // normally it's always the case
+            // ex Simone de Beauvoir
+            // beauvoir-simone
             if($arrNames['given'] != ''){
                 $res[] = $fam . ' ' . $arrNames['given'];
             }
-            else {
+            else{
                 $res[] = $fam;
+            }
+            // de-beauvoir-simone
+            if($arrNames['nobl'] != ''){
+                if($arrNames['given'] != ''){
+                    $res[] = $arrNames['nobl'] . ' ' . $fam . ' ' . $arrNames['given'];
+                }
+                else{
+                    $res[] = $arrNames['nobl'] . ' ' . $fam;
+                }
             }
         }
         // spouse
@@ -56,7 +65,7 @@ class Search {
             if($arrNames['given'] != ''){
                 $res[] = $spouse . ' ' . $arrNames['given'];
             }
-            else {
+            else{
                 $res[] = $spouse;
             }
         }
@@ -74,6 +83,57 @@ class Search {
             }
         }
         return $res;
+    }
+    
+    /**
+        Adds a new person in table search.
+    **/
+    public static function addPerson(Person $p) {
+        $dblink = DB5::getDbLink();
+        $stmt_insert = $dblink->prepare("insert into search(search_term,slug,day,name)values(?,?,?,?)");
+        $json_name = json_decode($p->data['name'], true);
+        $slug = $p->data['slug'];
+        $search_names = Search::computePersonNames($json_name);
+        $name = $p->getCommonName(); ///////////////// TODO Implement
+        $bday = substr($slug, -10);
+        foreach($search_names as $search_name){
+            $name_slug = slugify::compute($search_name);
+            $stmt_insert->execute([$name_slug, $slug, $bday, $name]);
+            $N_inserted++;
+        }
+    }
+    
+    /** 
+        Recomputes completely table search used by ajax, for all persons stored in database.
+        @param  $params empty array
+        @return report.
+    **/
+    public static function addAllPersons(): string {
+        $report = '';
+        $dblink = DB5::getDbLink();
+        $dblink->exec("delete from search");
+        $stmt_insert = $dblink->prepare("insert into search(search_term,slug,day,name)values(?,?,?,?)");
+        $N_person = 0;
+        $N_inserted = 0;
+        $t1 = microtime(true);
+        $query = "select slug,name from person";
+        $stmt = $dblink->query($query);
+        foreach($stmt->fetchAll(\PDO::FETCH_ASSOC) as $row){
+            $slug = $row['slug'];
+            $json_name = json_decode($row['name'], true);
+            $search_names = Search::computePersonNames($json_name);
+            $bday = substr($slug, -10);
+            foreach($search_names as $search_name){
+                $name_slug = slugify::compute($search_name);
+                $stmt_insert->execute([$name_slug, $slug, $bday, $search_name]);
+                $N_inserted++;
+            }
+            $N_person++;
+        }
+        $t2 = microtime(true);
+        $dt = round($t2 - $t1, 3);
+        $report .= "Inserted $N_inserted lines for $N_person persons ($dt s)\n";
+        return $report;
     }
     
 } // end class
--- src/model/db-create/search.sql
+++ src/model/db-create/search.sql
@@ -2,9 +2,9 @@
 -- used for ajax search of a person by birth day or by name
 
 create table search (
+    search_term varchar(255),
     slug        varchar(255), -- person slug
     day         char(10),     -- birth day, format YYYY-MM-DD
     name        varchar(255)  -- name of the corresponding person
 );
-create index search_day_idx on search(day);
-create index search_slug_idx on search(slug);
+create index search_term_idx on search(search_term);
